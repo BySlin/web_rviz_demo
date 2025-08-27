0ca5306aeece39b37d44dff45a1f8ba4a32ee78f
WrOve <2292735470@qq.com>
2025-08-27 17:20:13 +0800

    更新说明

---
+++ README.md
@@ -0,0 +1,28 @@
+# Vue 3 Web Rviz
+
+### 本项目仅供学习使用
+
+本项目基于Foxglove Bridge, 使用前请先安装并启动Foxglove bridge
+https://github.com/foxglove/ros-foxglove-bridge
+
+### 因某些公司拿此项目商用
+
+现在代码会每五分钟断开连接，并弹窗
+
+## 安装依赖
+
+```bash
+npm install
+```
+
+## 启动开发环境 访问地址http://localhost:5173
+
+```bash
+npm run dev
+```
+
+## 打包
+
+```bash
+npm run build
+```
---
+++ src/components/options/ConnectOptions.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+interface Props {
+  modelValue: {
+    server: string;
+    port: number;
+  };
+}
+
+const props = defineProps<Props>();
+
+defineEmits<{
+  (event: 'update:modelValue', value: any): void;
+}>();
+</script>
+
+<template>
+  <a-collapse-item header="Connect Options" key="ConnectOptions">
+    <a-form :model="modelValue" auto-label-width>
+      <a-form-item label="Server IP">
+        <a-input
+          :default-value="modelValue.server"
+          @change="
+            (v) =>
+              $emit('update:modelValue', { ...props.modelValue, server: v })
+          "
+        >
+        </a-input>
+      </a-form-item>
+      <a-form-item label="Server Port">
+        <a-input-number
+          :default-value="modelValue.port"
+          @change="
+            (v) => {
+              $emit('update:modelValue', {
+                ...props.modelValue,
+                port: v,
+              });
+            }
+          "
+        />
+      </a-form-item>
+    </a-form>
+  </a-collapse-item>
+</template>
+
+<style scoped></style>
